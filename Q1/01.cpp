57ca646c8ed536b841ad45bc028905fe5ddaed28
Ganesh <seeramdurgaganesh@gmail.com>
2024-12-21 22:02:31 +0530

    question 1

---
+++ Q1/01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    cin >> str1;
+    cin >> str2;
+
+    cout << "Concatenated string: " << str1+str2;
+
+    return 0;
+}

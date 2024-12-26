2fe073c4ef6f5a8dbc9ce17165ef51dcf4d4dea5
Ganesh <seeramdurgaganesh@gmail.com>
2024-12-26 23:10:07 +0530

    implement input handling and sorting for array in 01.cpp

--- Q5/01.cpp
+++ Q5/01.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int m;
+    cin >> m;
+
+    sort(arr, arr+n);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+    int subArr[m];
     
     return 0;
 }

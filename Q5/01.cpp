48a82a436411a513973fd106b40932567563eca7
Ganesh <seeramdurgaganesh@gmail.com>
2024-12-26 23:15:03 +0530

    calculate and output the difference between max and min values in the subarray

--- Q5/01.cpp
+++ Q5/01.cpp
@@ -17,11 +17,21 @@ int main()
 
     sort(arr, arr+n);
 
-    for(int i=0; i<n; i++){
-        cout << arr[i] << " ";
+    int subArr[m];
+    for(int i=0; i<m; i++){
+        subArr[i] = arr[i];
     }
 
-    int subArr[m];
-    
+    int min = INT32_MAX;
+    for(int i=0; i<m; i++){
+        if(subArr[i] < min) min = subArr[i];
+    }
+    int max = INT32_MIN;
+    for(int i=0; i<m; i++){
+        if(subArr[i] > max) max = subArr[i];
+    }
+
+    cout << max-min;
+
     return 0;
 }

e708e2328240366218d24544ce1441a0baccc665
Ganesh <seeramdurgaganesh@gmail.com>
2024-12-27 11:03:35 +0530

    implement array input and check for duplicate values in Q6/01.cpp

--- Q6/01.cpp
+++ Q6/01.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
     
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int j=n-1;
+    bool flag=false;
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+                flag=true;
+                break;
+            }
+        }
+    }
+
+    cout << flag;
     return 0;
 }

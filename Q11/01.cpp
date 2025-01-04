bf5690141d167ebc168198b2519ced73d0fbe8b3
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-04 22:19:07 +0530

    implement two-sum problem solution in Q11/01.cpp; add VSCode settings for C++ file associations

--- Q11/01.cpp
+++ Q11/01.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 int main()
 {
-    
+    int t;
+    cin >> t;
+
+    for(int k=0; k<t; k++){
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        
+        int target;
+        cin >> target;
+
+        bool flag = false;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] + arr[j] == target){
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        if(flag) cout << "YES" << endl;
+        else cout << "NO" << endl;
+
+    }
     return 0;
 }

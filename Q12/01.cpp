f9e283b7f2a804695ba3c2b88faf8379bb930aa9
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-06 23:35:22 +0530

    implement input handling and heap operations in main function of Q12/01.cpp

--- Q12/01.cpp
+++ Q12/01.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+
 using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    vector<int> v;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+
+    make_heap(v.begin(), v.end(), greater<int>());
+    pop_heap(v.begin(), v.end(), greater<int>());
+
     
+
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
     return 0;
 }

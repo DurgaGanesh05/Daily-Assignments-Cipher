d85af4a7bd4fc36b8b39c88951ce79281276f9a0
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-09 22:16:24 +0530

    implement logic to find triplets with sum zero in main function of Q13/01.cpp

--- Q13/01.cpp
+++ Q13/01.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
-    
+    int n = 6;
+    int arr[n] = {-1, 0, 1, 2, -1, 4};
+
+    cout << "Triplets with sum 0 are: " << endl;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            for(int k = j+1; k < n; k++){
+                if(arr[i] + arr[j] + arr[k] == 0) cout << arr[i] << " " << arr[j] << " " << arr[k] << endl;
+            }
+        }
+    }
     return 0;
 }

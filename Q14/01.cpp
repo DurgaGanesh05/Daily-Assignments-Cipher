0c263fc890191fb0be1bd995adb98d9a971d582f
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-10 22:24:00 +0530

    convert string to lowercase in main function of Q14/01.cpp

--- Q14/01.cpp
+++ Q14/01.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-    
+    string s = "A man, a plan, a canal: Panama";
+
+    for(int i=0; i<s.length(); i++){
+        s[i] = tolower(s[i]);
+    }
+
+    cout << s << endl;
     return 0;
 }

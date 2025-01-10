6147fde18b8e18698a589f57f219bd3c84800390
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-10 22:46:46 +0530

    implement palindrome checking logic in main function of Q14/01.cpp

--- Q14/01.cpp
+++ Q14/01.cpp
@@ -1,14 +1,40 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-    string s = "A man, a plan, a canal: Panama";
-
+bool palindrome(string s){
     for(int i=0; i<s.length(); i++){
         s[i] = tolower(s[i]);
     }
 
-    cout << s << endl;
+    for(int i=0; i<s.length(); i++){
+        if(s[i] < 'a' || s[i] > 'z'){
+            s.erase(i, 1);
+            i--;
+        }
+    }
+
+    string s1;
+
+    for(int i=0; i<s.length(); i++){
+        s1 = s[i] + s1;
+    }
+
+    for(int i=0; i<s.length(); i++){
+        if(s[i] != s1[i]){
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    
+    if(palindrome(s)) cout << "True";
+    else cout << "False";
+        
     return 0;
 }

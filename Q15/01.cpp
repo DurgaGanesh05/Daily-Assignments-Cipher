3a8688affecf26fd113e6f41325e5ee099d9e014
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-12 20:51:40 +0530

    refactor anagram function and update main to read input strings

--- Q15/01.cpp
+++ Q15/01.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 bool anagram(string s1, string s2){
-    if(s1.length() != s2.length()){
-        return false;
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    if(s1 == s2){
+        return true;
     }
     else{
-        for(int i=0; i<s1.length(); i++){
-            if(s1[i] != s2[i]){
-                return false;
-                break;
-            }
-        }
+        return false;
     }
-    return true;
 }
 
 
-int main()
-{
-    string s1 = "anagram";
-    // cin >> s1;
+int main(){
+    string s1;
+    cin >> s1;
     
-    string s2 = "nagaram";
-    // cin >> s2;
+    string s2;
+    cin >> s2;
 
-    cout << anagram(s1, s2);    
-
+    if(anagram(s1, s2)){
+        cout << "True";
+    }
+    else{
+        cout << "False";
+    }
     return 0;
 }

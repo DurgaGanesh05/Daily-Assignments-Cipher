c4cdc6fb86dc71fe5a29efb1789bd90e3c2f07c8
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-15 23:53:59 +0530

    implement validation function for parentheses and update main to read input

--- Q16/01.cpp
+++ Q16/01.cpp
@@ -1,8 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+bool isValid(string s) {
     
+    for (char c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            opened += c;
+        } else {
+            if (opened.empty()) return false;
+            char last = opened.back();
+            if ((c == ')' && last == '(') ||
+                (c == ']' && last == '[') ||
+                (c == '}' && last == '{')) {
+                opened.pop_back();
+            } else {
+                return false;
+            }
+        }
+    }
+    return opened.empty();
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (isValid(s)) {
+        cout << "True.\n";
+    } else {
+        cout << "False.\n";
+    }
+
     return 0;
 }

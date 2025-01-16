5bdd7c1b9b30ae4afeb91dc82289e5ce03b8dea3
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-17 00:01:00 +0530

    implement longestCommonPrefix function and update main for testing

--- Q17/01.cpp
+++ Q17/01.cpp
@@ -1,7 +1,30 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{   
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+    string prefix = strs[0];
+    for (size_t i = 1; i < strs.size(); ++i) {
+        for (size_t j = 0; j < prefix.length(); ++j) {
+            if (j >= strs[i].length() || strs[i][j] != prefix[j]) {
+                prefix = prefix.substr(0, j);
+                break;
+            }
+        }
+        if (prefix.empty()) return "";
+    }
+    return prefix;
+}
+
+
+
+int main() {
+    vector<string> strs = {"dog", "asd", "disddre"};
+    cout << longestCommonPrefix(strs) << endl;
+
     return 0;
 }
